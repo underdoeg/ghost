48ccbdbd586f580e01ab93909927157813fe5580
Philip Whitfield <public@underdoeg.com>
2011-07-20 18:02:22 +0200

    added postBuild scripts

---
+++ build/linux/postBuild64.sh
@@ -0,0 +1,5 @@
+# script to automatically copy the updated a file to the ofxFenster directory
+#
+# paths are hardcoded, feel free to change them, but please don't commit the modified file back to the master
+
+cp -f lib/libGhost64.a '../../../../openFrameworks/addons/ofxFenster/libs/ghost/lib/linux64/' 
